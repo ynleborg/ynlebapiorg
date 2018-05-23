45c5b0bbd8435699957fa92cc10b7131812f6fc1
Jacek Grobelny <jacek.grobelny@gmail.com>
2018-05-23 09:44:29 +0200

    dodanie leaderboardow do steam

--- src/main/java/pl/ynleborg/ynlebapiorg/leaderboard/TALeaderboardService.java
+++ src/main/java/pl/ynleborg/ynlebapiorg/leaderboard/TALeaderboardService.java
@@ -8,6 +8,7 @@ import pl.ynleborg.ynlebapiorg.mac.Achievement;
 
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -33,16 +34,20 @@ public class TALeaderboardService {
         List<Score> scores = new ArrayList<>();
         processURL(scores, date, "https://www.trueachievements.com/leaderboard.aspx?leaderboardid=7810");
         processURL(scores, date, "https://www.truetrophies.com/leaderboard.aspx?leaderboardid=393");
-        //processURL(scores, "https://truesteamachievements.com/leaderboard.aspx?leaderboardid=91");
+        processURL(scores, date, "https://www.truesteamachievements.com/leaderboard.aspx?leaderboardid=91");
         return scores;
     }
 
     private void processURL(List<Score> scores, String date, String address) throws IOException {
         URL url = new URL(address);
+        final URLConnection urlConnection = url.openConnection();
+        urlConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0");
+        urlConnection.connect();
         HtmlCleaner htmlCleaner = new HtmlCleaner();
-        TagNode rootTagNode = htmlCleaner.clean(url);
+        TagNode rootTagNode = htmlCleaner.clean(urlConnection.getInputStream());
         String platform = getPlatform(address);
         processLeaderboard(scores, rootTagNode, date, platform);
+        log.info("processed: {}", address);
     }
 
     private String getPlatform(String address) {
@@ -64,7 +69,11 @@ public class TALeaderboardService {
                 String userName = gamerTag.findElementByName("a", true).getText().toString();
                 String icon = e.findElementByName("img", true).getAttributeByName("src");
                 TagNode score1 = e.findElementByAttValue(CLASS, "score", true, true);
-                String score = score1.getText().toString().substring(0, score1.getText().toString().indexOf("("));
+                String score = score1.getText().toString().replace(",", "");
+                int parenthesisIndex = score.indexOf("(");
+                if (parenthesisIndex != -1) {
+                    score = score.substring(0, parenthesisIndex);
+                }
                 scores.add(Score.builder()
                         .userName(userName)
                         .icon("http:" + icon)
